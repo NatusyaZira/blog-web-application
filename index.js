c00ef622de53d1e1b2fbc546e83bb2509b3ffdf6
Nataliia Kovalova <natusya.zira@outlook.com>
2024-11-12 21:27:26 +0200

    Apply reference between pages

--- index.js
+++ index.js
@@ -7,19 +7,30 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let posts = [];
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
   });
 
-  app.get("/", function(req, res){
-    res.render("index.ejs", {
-      });
-  });
-  app.get("/compose", function(req, res){
-    res.render("compose.ejs", {
 
-    });
-  });
+app.get("/compose", (req, res) => {
+  res.render("compose.ejs");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server lounched on port ${port}.`);
